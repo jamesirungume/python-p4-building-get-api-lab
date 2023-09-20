744b3503fb73c24e92e145e7573ba3c9a72d2712
Your Name <jymohcooler@gmail.com>
2023-09-21 00:35:45 +0300

    Complete assigment

--- server/app.py
+++ server/app.py
@@ -20,19 +20,37 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = []
+    for bakery in Bakery.query.all():
+        bakery_dict = {
+            "name":bakery.name,
+            "id":bakery.id
+        }
+        bakeries.append(bakery_dict)
+    response = make_response(jsonify(bakeries,200))
+    response.headers["Content-Type"] = "application/json"
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    bakery_dict = bakery.to_dict()
+    response = make_response(jsonify(bakery_dict,200))
+    response.headers["Content-Type"] = "application/json"
+    return response
+
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    baked_list = [{"id":goods.id, "name":goods.name,"price":goods.price} for goods in baked_goods]
+    return jsonify(baked_list)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    expensive_baked = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    good_data = {"id":expensive_baked .id, "name":expensive_baked .name,"price":expensive_baked.price}
+    return jsonify(good_data)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
